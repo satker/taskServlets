8d2b1cafd133f1640393557813a5de1ea71894bf
Artem_Kunats <Artem_Kunats@epam.com>
2018-05-24 17:54:44 +0300

    init commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+@
+taskServlets.iml,8\3\835747eb95d13ecddf1ebda15a311f75ca7d4d0c
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,13 @@
-public class Main {
+import com.sun.deploy.net.HttpRequest;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
+@WebServlet
+public class Main extends HttpServlet {
+
+  @Override
+  protected void doGet (HttpRequest req, HttpServlet res) throws ServletException {
+
+  }
 }
